1f3eb93f278c118d1c00a4e49f5d657f97ecba1c
OganM <ogan.mancarcii@gmail.com>
2019-07-13 03:25:14 -0700

    shiny stuff

---
+++ R/shiny.R
@@ -0,0 +1,48 @@
+make_shiny = function(rdog){
+    rdog %>%
+        as.character %>%
+        stringr::str_extract_all('(?<=<script>)(.|\n)*?(?=</script>)') %>%
+        {.[[1]]} %>%
+        paste(collapse = '\n') ->jsCode
+
+    browser()
+
+    # fixerCode =
+    #     glue::glue(
+    #         "let canvas = document.getElementById('<attributes(rdog)$id>')
+    #         let parent = canvas.parentNode
+    #         parent.removeChild(canvas)
+    #         parent.appendChild(canvas)
+    #         ",
+    #         .open = '<',.close = '>')
+
+    shinyjs::runjs(paste0('\n',jsCode))
+
+    out = htmltools::tagList(rdog,
+                             htmltools::tags$script(jsCode))
+    return(out)
+
+}
+
+
+rdogOutput = function(id,
+                      class = id,
+                      height = 240,
+                      width = 240,
+                      background = '#FFDDBB'){
+
+
+    dependency = htmltools::htmlDependency(
+        'zdog',
+        src = system.file('zdog',package = 'rdog'),
+        version = '1.0',
+        script = c('zdog.min.js','zfont.min.js')
+    )
+
+    canvas = htmltools::tags$canvas(id = id, class = class,width=width, height = height, style = glue::glue("background:{background}"))
+
+    tagList(shinyjs::useShinyjs(),
+            dependency,
+            canvas)
+
+}
